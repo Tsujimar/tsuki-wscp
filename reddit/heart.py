fe0017d335bbe4691d5b204544b18e50385dbe6e
TsujimarDev <tsujimardev@gmail.com>
2023-02-22 00:42:25 +0100

    Critical bug fix
    * Fixed reddit rate limit crash

--- reddit/heart.py
+++ reddit/heart.py
@@ -1,9 +1,11 @@
 import json
 import time
-from urllib import request
+from urllib import request, error
 import psycopg2
 from dotenv import load_dotenv
 import os
+import random
+
 load_dotenv()
 
 postList = []
@@ -18,35 +20,38 @@ def gather():
 
     for post in get_posts:
         permalink = post['data'].get('permalink', None)
-        title = post['data'].get('title', None)
-        if permalink and title:
-            postList.append({'permalink': permalink, 'title': title})
+        if permalink:
+            postList.append({'permalink': permalink})
+    print(len(postList))
     crawl()
 
 
 def crawl():
-    max_count = 100
-    current_count = 0
     for link in postList:
         target = link['permalink']
         url2 = f'https://www.reddit.com{target}.json'
-        response = request.urlopen(url2)
+
+        while True:
+            try:
+                response = request.urlopen(url2)
+                break
+            except error.HTTPError as e:
+                if e.code == 429:
+                    print("Reached maximum requests, waiting and retrying...")
+                    time.sleep(random.randint(10, 20))
+                else:
+                    raise e
+
         json_data = json.loads(response.read().decode())
         get_children = json_data[1]['data']['children'][1:]
 
         for reply in get_children:
             scrap_messages = reply['data'].get('body', None)
             parentReply.append(scrap_messages)
-            current_count += 1
-            if current_count == max_count:
-                break
-        if current_count == max_count:
-            break
-        time.sleep(1)
+
+        time.sleep(random.randint(10, 20))
         print(parentReply)
         logData()
-        current_count = 0
-        time.sleep(60)
 
 
 def logData():

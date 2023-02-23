36f2f6fe8b4d1aeaf62178edddbb7c23dcf15911
TsujimarDev <tsujimardev@gmail.com>
2023-02-23 22:31:53 +0100

    Critical bug fix
    * Python error fix

--- reddit/heart.py
+++ reddit/heart.py
@@ -45,9 +45,14 @@ def crawl():
                 response = request.urlopen(url2)
                 break
             except (error.HTTPError, UnicodeEncodeError) as e:
-                if isinstance(e, error.HTTPError) or e.code == 429:
-                    print("Reached maximum requests, waiting and retrying...")
-                    time.sleep(random.randint(10, 20))
+                if isinstance(e, error.HTTPError):
+                    if e.code == 429:
+                        print("Reached maximum requests, waiting and retrying...")
+                        time.sleep(random.randint(10, 20))
+                    else:
+                        raise e
+                elif isinstance(e, UnicodeEncodeError):
+                    print("Encountered a UnicodeEncodeError: " + str(e) + ". Retrying...")
                 else:
                     raise e
 

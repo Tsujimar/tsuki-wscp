34bb93ba0a04705618eb2b48c3f6b80cb77bd1b7
TsujimarDev <tsujimardev@gmail.com>
2023-04-01 20:51:37 +0000

    Added sfw boards
    * Added only safe boards to scraper

--- 4chan/board.py
+++ 4chan/board.py
@@ -5,23 +5,25 @@ from urllib import request, error
 
 
 def crawler():
-    board = sys.argv[2]
+    # board = sys.argv[2]
+    boards = ['a', 'c', 'w', 'm', 'cgl', 'cm', 'lgbt', '3', 'adv', 'an', 'biz', 'cgl', 'ck', 'co', 'diy', 'fa', 'fit', 'gd', 'his', 'int', 'jp', 'lit', 'mlp', 'mu', 'n', 'news', 'out', 'po', 'pw', 'qst', 'sci', 'sp', 'tg', 'toy', 'trv', 'tv', 'vp', 'vt', 'wsg', 'wsr', 'x', 'xs']
     count = 0
     while count != 11:
-        try:
-            url = f'https://boards.4channel.org/{board}/{count}'
-            response = request.urlopen(url)
-            soup = BeautifulSoup(response, 'html.parser')
-            main_content = soup.find_all('div', id=re.compile("t"))
-            for i in range(len(main_content)):
-                head_line = main_content[i].find('span', attrs={'data-tip-cb': True})
-                post_message = main_content[i].find('blockquote', class_="postMessage")
-                if head_line is not None and post_message is not None:
-                    print(head_line.text, post_message.text)
-            count += 1
-        except error.HTTPError as e:
-            if e.code == 404:
-                count = 2
+        for board in boards:
+            try:
+                url = f'https://boards.4channel.org/{board}/{count}'
+                response = request.urlopen(url)
+                soup = BeautifulSoup(response, 'html.parser')
+                main_content = soup.find_all('div', id=re.compile("t"))
+                for i in range(len(main_content)):
+                    head_line = main_content[i].find('span', attrs={'data-tip-cb': True})
+                    post_message = main_content[i].find('blockquote', class_="postMessage")
+                    if head_line is not None and post_message is not None:
+                        print(head_line.text, post_message.text)
+                count += 1
+            except error.HTTPError as e:
+                if e.code == 404:
+                    count = 2
 
 
 crawler()

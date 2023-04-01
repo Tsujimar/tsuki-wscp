6a2dabfd4118ee487a10aa7999b7df0489fb1d34
TsujimarDev <tsujimardev@gmail.com>
2023-04-01 22:33:49 +0000

    Added Selenium virtualisation
    * The scraper can now go through ALL expanded comments in each post

--- 4chan/board.py
+++ 4chan/board.py
@@ -2,15 +2,24 @@ import re
 import sys
 from bs4 import BeautifulSoup
 from urllib import request, error
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
 
 
 def crawler():
     # board = sys.argv[2]
-    boards = ['a', 'c', 'w', 'm', 'cgl', 'cm', 'lgbt', '3', 'adv', 'an', 'biz', 'cgl', 'ck', 'co', 'diy', 'fa', 'fit', 'gd', 'his', 'int', 'jp', 'lit', 'mlp', 'mu', 'n', 'news', 'out', 'po', 'pw', 'qst', 'sci', 'sp', 'tg', 'toy', 'trv', 'tv', 'vp', 'vt', 'wsg', 'wsr', 'x', 'xs']
-    count = 0
-    while count != 11:
-        for board in boards:
+    driver = webdriver.Firefox()
+    boards = ['a', 'c', 'w', 'm', 'cgl', 'cm', 'lgbt', '3', 'adv', 'an', 'biz', 'cgl', 'ck', 'co', 'diy', 'fa', 'fit',
+              'gd', 'his', 'int', 'jp', 'lit', 'mlp', 'mu', 'n', 'news', 'out', 'po', 'pw', 'qst', 'sci', 'sp', 'tg',
+              'toy', 'trv', 'tv', 'vp', 'vt', 'wsg', 'wsr', 'x', 'xs']
+    for board in boards:
+        count = 0
+        while count != 11:
             try:
+                # Scrape posts
                 url = f'https://boards.4channel.org/{board}/{count}'
                 response = request.urlopen(url)
                 soup = BeautifulSoup(response, 'html.parser')
@@ -19,11 +28,31 @@ def crawler():
                     head_line = main_content[i].find('span', attrs={'data-tip-cb': True})
                     post_message = main_content[i].find('blockquote', class_="postMessage")
                     if head_line is not None and post_message is not None:
-                        print(head_line.text, post_message.text)
+                        hl_t = head_line.get_text()
+                        pm_t = post_message.get_text()
+                        print(hl_t, pm_t)
+                # Scrape comments
+                driver.get(url)
+                toggle_buttons = WebDriverWait(driver, 5).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[data-cmd='expand']"))
+                )
+                for toggle_button in toggle_buttons:
+                    toggle_button.click()
+                    WebDriverWait(driver, 5).until(
+                        EC.presence_of_element_located(
+                            (By.CSS_SELECTOR, "[src='//s.4cdn.org/image/buttons/burichan/post_expand_minus.png']"))
+                    )
+                    html = driver.page_source
+                    soup = BeautifulSoup(html, "html.parser")
+                    comments = soup.find_all("div", class_="postContainer replyContainer rExpanded")
+                    for comment in comments:
+                        print(comment.get_text())
                 count += 1
             except error.HTTPError as e:
                 if e.code == 404:
                     count = 2
+            except WebDriverException as e:
+                count = 2
 
 
 crawler()

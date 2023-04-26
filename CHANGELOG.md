4d8adbdbc4afd8433fae98f3639a5fe00696272f
TsujimarDev <tsujimardev@gmail.com>
2023-04-26 22:38:07 +0100

    Fixed major script breaking bug for data source : 4Chan
    * Selenium now uses base Firefox instead of developer browser under unknown (for now) circumstances, which caused 4Chan harvester to not work

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Changelog
-## [0.2.0] - 2023-04-27
+## [0.2.0] - 2023-04-26
 ### Added
 - New data source: Twitter
 - Twitter harvester added to threading
@@ -8,4 +8,5 @@
 - bashrc requirements to mandate twitter credentials
 
 ### Fixed
+- Script breaking bug from 4Chan
 - Version number
--- fourchan/board.py
+++ fourchan/board.py
@@ -54,58 +54,61 @@ def crawler(delay, is_nsfw):
 
         for board in default_boards:
             count = 0
-            while count != 11:
-                # Scrape posts
-                url = f'https://boards.4channel.org/{board}/{count}'
-                response = request.urlopen(url)
-                soup = BeautifulSoup(response, 'html.parser')
-                main_content = soup.find_all('div', id=re.compile("t"))
-                for i in range(len(main_content)):
-                    post_message = main_content[i].find('blockquote', class_="postMessage")
-                    if post_message is not None:
-                        pm_t = post_message.get_text()
-                        cur.execute('SELECT * FROM "wscp_data" WHERE "message" = %s',
-                                    (pm_t,))
-                        rows = cur.fetchall()
-                        if not rows:
-                            print(style.YELLOW + "[4Chan]", end='')
-                            print(style.GREEN + "Added", end=' ')
-                            print(style.RESET + pm_t)
-                            cur.execute('INSERT INTO "wscp_data" ("message", "source") VALUES (%s, %s)',
-                                        (pm_t, "4Chan"))
-                    conn.commit()
-                # Scrape comments
-                driver.get(url)
-                toggle_buttons = WebDriverWait(driver, delay).until(
-                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[data-cmd='expand']"))
-                )
-                for toggle_button in toggle_buttons:
-                    toggle_button.click()
-                    WebDriverWait(driver, delay).until(
-                        EC.presence_of_element_located(
-                            (By.CSS_SELECTOR, "[src='//s.4cdn.org/image/buttons/burichan/post_expand_minus.png']"))
-                    )
-                    html = driver.page_source
-                    soup = BeautifulSoup(html, "html.parser")
-                    comments = soup.find_all("div", class_="postContainer replyContainer rExpanded")
-                    for comment in comments:
-                        comment = comment.find("blockquote", class_="postMessage")
-                        refurnished = comment.get_text()
-                        new_text = re.sub(
-                            r"No.\d{2,}▶|>>\d{2,}|File:.*.(jpg|png|webm|gif)|\d{2}/\d{2}/\d{2}|(.*\w)\d."
-                            r"*(JPG|PNG|WEBM|GIF)|\b\w+\s\(\w{2,}\)\d{2}:\d{2}:\d{2}\b|.*(OP).|>", "", refurnished)
-                        if len(new_text) != 0:
-                            cur.execute('SELECT * FROM "wscp_data" WHERE "message" = %s', (new_text,))
+
+            def spider(count):
+                while count != 11:
+                    # Scrape posts
+                    url = f'https://boards.4channel.org/{board}/{count}'
+                    response = request.urlopen(url)
+                    soup = BeautifulSoup(response, 'html.parser')
+                    main_content = soup.find_all('div', id=re.compile("t"))
+                    for i in range(len(main_content)):
+                        post_message = main_content[i].find('blockquote', class_="postMessage")
+                        if post_message is not None:
+                            pm_t = post_message.get_text()
+                            cur.execute('SELECT * FROM "wscp_data" WHERE "message" = %s',
+                                        (pm_t,))
                             rows = cur.fetchall()
                             if not rows:
                                 print(style.YELLOW + "[4Chan]", end='')
                                 print(style.GREEN + "Added", end=' ')
-                                print(style.RESET + new_text)
+                                print(style.RESET + pm_t)
                                 cur.execute('INSERT INTO "wscp_data" ("message", "source") VALUES (%s, %s)',
-                                            (new_text, "4Chan"))
+                                            (pm_t, "4Chan"))
                         conn.commit()
-                count += 1
+                    # Scrape comments
+                    driver.get(url)
+                    toggle_buttons = WebDriverWait(driver, delay).until(
+                        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[data-cmd='expand']"))
+                    )
+                    for toggle_button in toggle_buttons:
+                        toggle_button.click()
+                        WebDriverWait(driver, delay).until(
+                            EC.presence_of_element_located(
+                                (By.CSS_SELECTOR, "[src='//s.4cdn.org/image/buttons/burichan/post_expand_minus.png']"))
+                        )
+                        html = driver.page_source
+                        soup = BeautifulSoup(html, "html.parser")
+                        comments = soup.find_all("div", class_="postContainer replyContainer rExpanded")
+                        for comment in comments:
+                            comment = comment.find("blockquote", class_="postMessage")
+                            refurnished = comment.get_text()
+                            new_text = re.sub(
+                                r"No.\d{2,}▶|>>\d{2,}|File:.*.(jpg|png|webm|gif)|\d{2}/\d{2}/\d{2}|(.*\w)\d."
+                                r"*(JPG|PNG|WEBM|GIF)|\b\w+\s\(\w{2,}\)\d{2}:\d{2}:\d{2}\b|.*(OP).|>", "", refurnished)
+                            if len(new_text) != 0:
+                                cur.execute('SELECT * FROM "wscp_data" WHERE "message" = %s', (new_text,))
+                                rows = cur.fetchall()
+                                if not rows:
+                                    print(style.YELLOW + "[4Chan]", end='')
+                                    print(style.GREEN + "Added", end=' ')
+                                    print(style.RESET + new_text)
+                                    cur.execute('INSERT INTO "wscp_data" ("message", "source") VALUES (%s, %s)',
+                                                (new_text, "4Chan"))
+                            conn.commit()
+                    count += 1
 
+            spider(count)
     except Exception as e:
         if type(e) == KeyError:
             print(style.YELLOW + "[4Chan]", end='')
@@ -122,8 +125,10 @@ def crawler(delay, is_nsfw):
         elif type(e) == error.HTTPError:
             if e.code == 404:
                 count = 2
+                spider(count)
         elif type(e) == WebDriverException:
             count = 2
+            spider(count)
 
 
 def call_crawler(delay, is_nsfw):
